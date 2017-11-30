a8877ed34f73d95710f7344f49857963b369b722
webmasterdevlin <webmasterdevlin@gmail.com>
2017-11-30 23:04:53 +0800

    Add project files.

---
+++ AcrUserDialogsDemo/AcrUserDialogsDemo.Android/MainActivity.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Acr.UserDialogs;
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace AcrUserDialogsDemo.Droid
+{
+    [Activity(Label = "AcrUserDialogsDemo", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+
+            base.OnCreate(bundle);
+            UserDialogs.Init(this);
+            global::Xamarin.Forms.Forms.Init(this, bundle);
+            LoadApplication(new App());
+        }
+    }
+}
+
---
+++ AcrUserDialogsDemo/AcrUserDialogsDemo/MainPage.xaml.cs
@@ -0,0 +1,56 @@
+﻿using Acr.UserDialogs;
+using System;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Color = System.Drawing.Color;
+
+namespace AcrUserDialogsDemo
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_OnClicked(object sender, EventArgs e)
+        {
+            using (IProgressDialog progress = UserDialogs.Instance.Progress("Progress", null, null, true, MaskType.Black))
+            {
+                for (int i = 0; i < 100; i++)
+                {
+                    progress.PercentComplete = i;
+                    await Task.Delay(60);
+                }
+            }
+
+            using (UserDialogs.Instance.Loading("Loading", null, null, true, MaskType.Black))
+            {
+                await Task.Delay(5000);
+            }
+
+            UserDialogs.Instance.ShowError("ShowError (Obselete)", 3000); //Use ShowImage instead
+            
+            await Task.Delay(4000);
+
+            UserDialogs.Instance.ShowSuccess("ShowSuccess (Obselete)", 3000); //Use ShowImage instead
+
+            await Task.Delay(4000);
+
+            Toast();
+
+            await Task.Delay(5000);
+
+            await UserDialogs.Instance.DatePromptAsync("DatePrompt", DateTime.Now);
+        }
+
+        private static void Toast()
+        {
+            ToastConfig toastConfig = new ToastConfig("Toast");
+            toastConfig.SetDuration(3000);
+            toastConfig.SetBackgroundColor(Color.DimGray);
+
+            UserDialogs.Instance.Toast(toastConfig);
+        }
+    }
+}
